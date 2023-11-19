a6547dd454d23307f9d64b832abc62e7a370d822
jgutierrez9891 <j.gutierrezl@uniandes.edu.co>
2023-11-19 00:26:00 -0500

    Colecciones de postman

--- src/ciudad-supermercado/ciudad-supermercado.controller.ts
+++ src/ciudad-supermercado/ciudad-supermercado.controller.ts
@@ -11,28 +11,28 @@ import { SupermercadoEntity } from 'src/supermercado/supermercado.entity';
 export class CiudadSupermercadoController {
     constructor(private readonly ciudadSupermercadoService: CiudadSupermercadoService) {}
 
-    @Post('cities/:ciudadId/supermarkets/:supermercadoId')
+    @Post(':ciudadId/supermarkets/:supermercadoId')
     async addSupermarketToCity(@Param('ciudadId') ciudadId: string, @Param('supermercadoId') supermercadoId: string){
         return await this.ciudadSupermercadoService.addSupermarketToCity(ciudadId, supermercadoId);
     }
 
-    @Get('cities/:ciudadId/supermarkets/:supermercadoId')
+    @Get(':ciudadId/supermarkets/:supermercadoId')
     async findSupermarketFromCity(@Param('ciudadId') ciudadId: string, @Param('supermercadoId') supermercadoId: string){
         return await this.ciudadSupermercadoService.findSupermarketFromCity(ciudadId, supermercadoId);
     }
 
-    @Get('cities/:ciudadId/supermarkets')
+    @Get(':ciudadId/supermarkets')
     async findSupermarketsFromCity(@Param('ciudadId') ciudadId: string){
         return await this.ciudadSupermercadoService.findSupermarketsFromCity(ciudadId);
     }
 
-    @Put('cities/:ciudadId/supermarkets')
+    @Put(':ciudadId/supermarkets')
     async updateSupermarketsFromCity(@Body() supermercadosDto: SupermercadoDto[], @Param('ciudadId') ciudadId: string){
         const supermercados = plainToInstance(SupermercadoEntity, supermercadosDto)
         return await this.ciudadSupermercadoService.updateSupermarketsFromCity(ciudadId, supermercados);
     }
 
-    @Delete('cities/:ciudadId/supermarkets/:supermercadoId')
+    @Delete(':ciudadId/supermarkets/:supermercadoId')
     @HttpCode(204)
     async deleteSupermarketFromCity(@Param('ciudadId') ciudadId: string, @Param('supermercadoId') supermercadoId: string){
        return await this.ciudadSupermercadoService.deleteSupermarketFromCity(ciudadId, supermercadoId);
